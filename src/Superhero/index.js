fc662bbaf743713db3931df541db1b556096b65b
Marian <ilie.marian32@yahoo.com>
2022-01-14 16:05:27 +0000

    ammend

--- src/Superhero/index.js
+++ src/Superhero/index.js
@@ -1,32 +1,47 @@
 import { useEffect, useState } from "react";
 
 export function Superhero({ onSubmit }) {
+    const [dataResults, setDataResults] = useState([])
     const [superhero, setSuperhero] = useState("")
+    const [submited, setSubmited] = useState(false)
     useEffect(() => {
-        async function getData() {
-            const response = await fetch("https://superheroapi.com/api/4369296219843776/search/batman")
-            const data = await response.json();
-            setSuperhero(data)
-            console.log(data)
+        if (submited) {
+            async function getData() {
+                const response = await fetch(`https://superheroapi.com/api/4369296219843776/search/${superhero}`)
+                const data = await response.json();
+                setDataResults(data.results)
+                console.log(data.results)
+                console.log(superhero)
+            }
+            console.log("helloo ")
+            getData()
 
         }
-        getData()
-    }, [])
+
+    }, [submited])
     function handleChange(e) {
         setSuperhero(e.target.value)
     }
-    function onSubmit() {
-     
-          
-    }
-    return superhero ? (
+
+    return (
         <div className="App">
             <h1>SuperHeroes</h1>
-            <input type="text" placeholder="search" onChange={handleChange} ></input>
-            <button onClick={() => { onSubmit(superhero) }}></button>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                console.log("hi again")
+                setSubmited(true)
+            }}>
+                <input type="text" placeholder="search" onChange={handleChange} ></input>
+                <button> Search </button>
+            </form>
+            <div>
+
+                {!submited ? "" : (
+                    dataResults.map((item, index) => (
+                        <p>{key={ item.id} , data = item[index]}</p>
+                    )))}
+            </div>
         </div>
-        
-    ) : (
-        <></>
-    );
+    )
 }
+
